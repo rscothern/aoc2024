43bffdfe52a0d4158431db64171fe7e1f1e1eb0c
Richard Scothern <rsco@fb.com>
2024-12-07 13:19:08 +0000

    [aoc2024] day2 with real input

--- day2.py
+++ day2.py
@@ -1,36 +1,42 @@
 #!/usr/local/bin/python3
 
-def day2():
-    levels = [
-        [7, 6, 4, 2, 1],
-        [1, 2, 7, 8, 9],
-        [9, 7, 6, 2, 1],
-        [1, 3, 2, 4, 5],
-        [8, 6, 4, 4, 1],
-        [1, 3, 6, 7, 9]
-    ]
+test_levels = [
+    [7, 6, 4, 2, 1],
+    [1, 2, 7, 8, 9],
+    [9, 7, 6, 2, 1],
+    [1, 3, 2, 4, 5],
+    [8, 6, 4, 4, 1],
+    [1, 3, 6, 7, 9]
+]
 
+def day2():
+            
     def process_level(level) -> bool:
         should_increase = level[1] > level[0]
-        last = level[0] - 1
+        last = level[0] - 1 if should_increase else level[0] + 1
+        
         for i, num in enumerate(level):
             if abs(num-last) not in [1,2,3]:
-                print(f"Level bad, delta too large: {num-last=}, {level=}")
                 return False
-            if level[1] < level[0] and should_increase:
-                print(f"Level bad, {should_increase=}: {level=}")
+            if num < last and should_increase:
                 return False
-            if level[1] > level[0] and not should_increase:
-                print(f"Level bad, {should_increase=}: {level=}")
+            if num > last and not should_increase:
                 return False
 
             last = num
             
-        print(f"Level ok {level=}")
         return True
         
-    for level in levels:
-        print(f"Processing {level}")
-        process_level(level)
+    levels = []
+    with open("day2.txt") as f:
+        for line in f.readlines():
+            levels.append([int(num) for num in line.split(" ")])
 
-day2()
+    num_safe = 0
+    for level in levels:
+        if process_level(level):
+            num_safe += 1
+
+    return num_safe
+
+print(day2())

d01285f681409e86948129f1b19dc42c12f64238
Richard Scothern <rsco@fb.com>
2024-12-07 12:50:21 +0000

    [aoc2024] day3 with real input

--- day3.py
+++ day3.py
@@ -1,8 +1,12 @@
 #!/usr/local/bin/python3
 import re
 
+test_mem = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 def day3():
-    mem = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    with open("day3.txt") as f:
+        mem = f.read()
+
+
     pattern = "mul\((\d+),(\d+)\)"
     prog = re.compile(pattern)
     matches = prog.finditer(mem)
